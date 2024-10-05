06cf639b23ec0b6f30cac7e58c4eaee773116df2
xurel <151750472+xurel7@users.noreply.github.com>
2024-10-05 18:39:12 -0400

    Update Basketball-Legends.js

--- Bot/Games/Basketball-Legends.js
+++ Bot/Games/Basketball-Legends.js
@@ -1,22 +1,22 @@
 {
     "status": "online",
-    "warnings": "Certain features like speed modifier, dribble boost, and anti contest are/can be blatant",
+    "warnings": "Certain features like speed modifier, dribble boost, and anti contest are/can be blatant.",
     "features": [
+        {
+            "name": "Anti Contest",
+            "description": "Eliminates contest when shooting."
+        },
         {
             "name": "Auto Green",
             "description": "Allows you to instantly green your shots."
-        },
-        {
-            "name": "Speed Modifier",
-            "description": "Allows you to change your speed."
         },
         {
             "name": "Dribble Boost",
             "description": "Allows you to boost the distance you travel while dribbling."
         },
         {
-            "name": "Anti Contest",
-            "description": "Elimates contest when shooting."
+            "name": "Speed Modifier",
+            "description": "Allows you to change your speed."
         }
     ]
 }
