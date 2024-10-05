99a5775fcd3c329872ce400a0de3348ea133c648
xurel <151750472+xurel7@users.noreply.github.com>
2024-10-05 18:39:53 -0400

    Update Universe-Football.js

--- Bot/Games/Universe-Football.js
+++ Bot/Games/Universe-Football.js
@@ -3,36 +3,36 @@
     "warnings": "Some of these features can be blatant. Proceed with caution, as the Universe Football moderation team is efficient.",
     "features": [
         {
-            "name": "Instant Touchdown",
-            "description": "Grants you an instant touchdown."
-        },
-        {
-            "name": "Instant Tackle",
-            "description": "Allows you to instantly tackle the ball carrier."
+            "name": "Anti Tackle",
+            "description": "Prevents you from being tackled."
         },
         {
             "name": "Infinite Stamina",
             "description": "Allows you to never run out of stamina."
         },
         {
-            "name": "Anti Tackle",
-            "description": "Prevents you from being tackled."
+            "name": "Instant Tackle",
+            "description": "Allows you to instantly tackle the ball carrier."
         },
         {
-            "name": "Speed Modifier",
-            "description": "Allows you to change your speed."
+            "name": "Instant Touchdown",
+            "description": "Grants you an instant touchdown."
         },
         {
             "name": "Jump Modifier",
             "description": "Allows you to change how high you jump."
         },
         {
+            "name": "Remove Block Effect",
+            "description": "Eliminates the pushback you receive when blocked."
+        },
+        {
             "name": "Remove Jump Cooldown",
             "description": "Removes the cooldown for jumping."
         },
         {
-            "name": "Remove Block Effect",
-            "description": "Eliminates the pushback you receive when blocked."
+            "name": "Speed Modifier",
+            "description": "Allows you to change your speed."
         }
     ]
 }
