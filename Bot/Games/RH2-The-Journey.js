4a15638edc2369c8fc10531a995aeddbd9853c9e
xurel <151750472+xurel7@users.noreply.github.com>
2024-10-06 12:42:24 -0400

    Create RH2-The-Journey.js

---
+++ Bot/Games/RH2-The-Journey.js
@@ -0,0 +1,34 @@
+{
+    "status": "online",
+    "warnings": "None",
+    "features": [
+        {
+            "name": "Anti Contest",
+            "description": "Eliminates contest when shooting."
+        },
+        {
+            "name": "Auto Green",
+            "description": "Allows you to instantly green your shots."
+        },
+        {
+            "name": "Ball Magnet",
+            "description": "Allows you to get a bigger hitbox while catching/picking up the ball."
+        },
+        {
+            "name": "Dribble Boost",
+            "description": "Allows you to boost the distance you travel while dribbling."
+        },
+        {
+            "name": "Dribble Macros",
+            "description": "Allows you to set keybinds for dribble moves."
+        },
+        {
+            "name": "Infinite Stamina",
+            "description": "Allows you to never run out of stamina."
+        },
+        {
+            "name": "Speed Multiplier",
+            "description": "Multiplies your speed by a selected amount."
+        }
+    ]
+}
