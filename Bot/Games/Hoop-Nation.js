eb000b0f7a6842216a7a64112ddcb3845c755009
xurel <151750472+xurel7@users.noreply.github.com>
2024-10-06 12:27:32 -0400

    Create Hoop-Nation.js

---
+++ Bot/Games/Hoop-Nation.js
@@ -0,0 +1,34 @@
+{
+    "status": "online",
+    "warnings": "None",
+    "features": [
+        {
+            "name": "Auto Green",
+            "description": "Allows you to instantly green your shots."
+        },
+        {
+            "name": "Dribble Boost",
+            "description": "Allows you to boost the distance you travel while dribbling."
+        },
+        {
+            "name": "Dribble Macros",
+            "description": "Allows you to set keybinds for dribble moves."
+        },
+        {
+            "name": "Free Gamepasses",
+            "description": "Grants you animation gamepasses for free."
+        },
+        {
+            "name": "Infinite Stamina",
+            "description": "Allows you to never run out of stamina."
+        },
+        {
+            "name": "Rep Locked Stores Bypass",
+            "description": "Allows you to purchase items from stores that would be rep locked."
+        },
+        {
+            "name": "Speed Modifier",
+            "description": "Allows you to change your speed."
+        }
+    ]
+}
