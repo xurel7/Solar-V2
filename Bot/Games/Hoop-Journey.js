2529d052d22ee2eb3e936a1b57e5850f808e02b8
xurel <151750472+xurel7@users.noreply.github.com>
2024-10-06 12:17:59 -0400

    Update Hoop-Journey.js

--- Bot/Games/Hoop-Journey.js
+++ Bot/Games/Hoop-Journey.js
@@ -1,1 +1,18 @@
-
+{
+    "status": "online",
+    "warnings": "None",
+    "features": [
+        {
+            "name": "Auto Green",
+            "description": "Allows you to instantly green your shots."
+        },
+        {
+            "name": "Dribble Macros",
+            "description": "Allows you to set keybinds for dribble moves."
+        },
+        {
+            "name": "Infinite Stamina",
+            "description": "Allows you to never run out of stamina."
+        }
+    ]
+}
