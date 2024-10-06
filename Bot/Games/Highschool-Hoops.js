17d31dae025f41a258a21e656c9646d9c3bf3ab3
xurel <151750472+xurel7@users.noreply.github.com>
2024-10-06 12:33:40 -0400

    Create Highschool-Hoops.js

---
+++ Bot/Games/Highschool-Hoops.js
@@ -0,0 +1,22 @@
+{
+    "status": "online",
+    "warnings": "None",
+    "features": [
+        {
+            "name": "Anti Contest",
+            "description": "Eliminates contest when shooting."
+        },
+        {
+            "name": "Auto Green",
+            "description": "Allows you to instantly green your shots."
+        },
+        {
+            "name": "Dribble Boost",
+            "description": "Allows you to boost the distance you travel while dribbling."
+        },
+        {
+            "name": "Dribble Macros",
+            "description": "Allows you to set keybinds for dribble moves."
+        }
+    ]
+}
