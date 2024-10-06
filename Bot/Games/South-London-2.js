aa8ab05a6a19c3f5251671510195393c4c8a7752
xurel <151750472+xurel7@users.noreply.github.com>
2024-10-06 13:08:27 -0400

    Create South-London-2.js

---
+++ Bot/Games/South-London-2.js
@@ -0,0 +1,86 @@
+{
+    "status": "online",
+    "warnings": "None",
+    "features": [
+        {
+            "name": "Admin Item Bypass",
+            "description": "Allows you to equip and use admin items."
+        },
+        {
+            "name": "Allow Jumping",
+            "description": "Allows you to jump."
+        },
+        {
+            "name": "Apartment Bypass",
+            "description": "Allows you to equip and shoot guns in apartments."
+        },
+        {
+            "name": "Car Mod",
+            "description": "Allows you to mod your vehicle."
+        },
+        {
+            "name": "ESP",
+            "description": "Allows you to see people through walls."
+        },
+        {
+            "name": "Full Auto Conversion",
+            "description": "Allows you to convert semi auto weapons to full auto."
+        },
+        {
+            "name": "Hitbox Extender",
+            "description": "Extends all players' hitboxes."
+        },
+        {
+            "name": "Infinite Ammo",
+            "description": "Allows you to never run out of ammo."
+        },
+        {
+            "name": "Infinite Gas",
+            "description": "Allows you to never run out of gas."
+        },
+        {
+            "name": "Infinite Hunger",
+            "description": "Allows you to never run out of hunger."
+        },
+        {
+            "name": "Infinite Skittles",
+            "description": "Gives you the Skittles effect forever."
+        },
+        {
+            "name": "Infinite Stamina",
+            "description": "Allows you to never run out of stamina."
+        },
+        {
+            "name": "Max Safe Storage",
+            "description": "Allows you to get max safe storage."
+        },
+        {
+            "name": "Noclip",
+            "description": "Allows you to move through walls."
+        },
+        {
+            "name": "Pick Up Items",
+            "description": "Allows you to pick up dropped items."
+        },
+        {
+            "name": "Push Aura",
+            "description": "Allows you to push everyone close to you. (Fists required)"
+        },
+        {
+            "name": "Remove Recoil",
+            "description": "Removes all recoil while shooting weapons."
+        },
+        {
+            "name": "Save Guns",
+            "description": "If dead, all your guns will be automatically stored in your safe."
+        },
+        {
+            "name": "Silent Aim",
+            "description": "Allows you to shoot players without aiming directly at them."
+        },
+        {
+            "name": "Speed Modifier",
+            "description": "Allows you to change your speed."
+        }
+    ]
+}
